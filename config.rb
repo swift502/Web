a7df3739a5b756afbbdc65fdabaf2d72172a9fac
swift502 <blaha.j502@gmail.com>
2020-07-16 21:56:58 +0200

    pretty urls

--- config.rb
+++ config.rb
@@ -7,8 +7,11 @@ page '/*.txt', layout: false
 # Source directory for the web server
 set :source, 'src'
 
-# Build config
+configure :development do
+    activate :directory_indexes
+end
+
 configure :build do
-  activate :minify_css
-  activate :minify_javascript
+    activate :minify_css
+    activate :minify_javascript
 end
