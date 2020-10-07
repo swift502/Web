c1d8678909fe3bfa27e33d70a75e787eb2148fad
swift502 <blaha.j502@gmail.com>
2020-10-07 20:14:07 +0200

    homepage v3

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -1,3 +1,17 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction()
+{
+	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0)
+	{
+		document.getElementById("navbar").style.borderColor = "#ccc";
+	}
+	else
+	{
+		document.getElementById("navbar").style.borderColor = "#fff";
+	}
+}
+
 function emailToClipboard()
 {
 	const el = document.createElement('textarea');
