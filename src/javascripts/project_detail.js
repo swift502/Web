f99d900abff5d9f723cf66cd372249139284d314
swift502 <blaha.j502@gmail.com>
2020-10-28 20:32:59 +0100

    page title fix, video description fix, responsivness

--- src/javascripts/project_detail.js
+++ src/javascripts/project_detail.js
@@ -3,10 +3,8 @@ function onWindowResize()
 	var descs = document.getElementsByClassName("content-description");
 
 	for (var i = 0; i < descs.length; i++) {
-		if (descs[i].dataset.target) {
-			var width = document.getElementById(descs[i].dataset.target).offsetWidth;
-			descs[i].style.width = (width - 64) + 'px';
-		}
+		var width = document.getElementById(descs[i].dataset.target).offsetWidth;
+		descs[i].style.width = (width - 64) + 'px';
 	};
 }
 
