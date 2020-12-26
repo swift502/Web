51f040353982abd436c49c7bd74132ece24b7ed2
swift502 <blaha.j502@gmail.com>
2020-12-26 14:38:53 +0100

    homepage remade

--- src/javascripts/global.js
+++ src/javascripts/global.js
@@ -30,5 +30,5 @@ window.addEventListener('scroll', scrollFunction);
 
 if(window.navigator.userAgent.match(/(MSIE|Trident)/))
 {
-	alert("You're using an outdated web browser and the website will not display as intended. Please upgrade to a modern web browser to view the website properly.");
+	alert("You seem to be using an outdated web browser and the website may not display correctly. Please view this website in a modern web browser for it to display as intended.");
 }
--- src/javascripts/homepage.js
+++ src/javascripts/homepage.js
@@ -1,21 +1,9 @@
-// Model viewer border style
-const modelViewer = document.querySelector('model-viewer');
-modelViewer.addEventListener('load', function(evt)
-	{
-		document.getElementById('model-viewer-wrapper').style.borderColor = '#fff';
-	}
-);
-modelViewer.addEventListener('model-visibility', function(evt)
-	{
-		document.getElementById('model-viewer-wrapper').style.borderColor = '#fff';
-	}
-);
-
+// Load random model
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+}
 
-// Load random model
+const modelViewer = document.querySelector('model-viewer');
 modelNumber = getRandomInt(1, 9);
 modelPath = 'models/' + modelNumber + '.glb';
 modelViewer.setAttribute("src", modelPath);
