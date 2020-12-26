50ab5e45c0633da035b979f8f48b8fd95ad33c91
swift502 <blaha.j502@gmail.com>
2020-12-26 12:54:49 +0100

    tweaks

--- src/javascripts/homepage.js
+++ src/javascripts/homepage.js
@@ -1,76 +1,4 @@
-var aText = [
-"3d modelling",
-"texturing",
-"rigging",
-"animation",
-"python",
-"blender",
-"gimp",
-"c#",
-"hlsl",
-"unity",
-"html",
-"css",
-"javascript",
-"typescript",
-"web"
-];
-
-var iSpeed = 100; 
-var iIndex = 0; 
-var iArrLength = aText[0].length; 
-var iTextPos = 0; 
-	
-function typewriter()
-{
-	var destination = document.getElementById("typewriter");
-
-	destination.innerHTML = aText[iIndex].substring(0, iTextPos) + "_";
-	if (iTextPos++ == iArrLength)
-	{
-		iTextPos = 0;
-		const archiveText = aText[iIndex];
-		if (++iIndex == aText.length) iIndex = 0;
-		iArrLength = aText[iIndex].length;
-		setTimeout(function() {
-			pushArchive(archiveText);
-			typewriter();
-		}, 500);
-	} else
-	{
-		setTimeout(typewriter, iSpeed);
-	}
-}
-
-typewriter();
-
-const archive = [];
-
-function pushArchive(str)
-{
-	var div = document.createElement('div');
-	div.innerText = str;
-	document.getElementById('typewriter-history').appendChild(div);
-	archive.unshift(div);
-
-	refreshTransparency();
-}
-
-function refreshTransparency()
-{
-	for (var i = 0; i < archive.length; i++)
-	{
-		var opacity = 1 - (0.25 * (i + 1));
-		archive[i].style.opacity = opacity;
-	}
-
-	while (archive[archive.length - 1].style.opacity <= 0)
-	{
-		archive[archive.length - 1].outerHTML = "";
-		archive.pop();
-	}
-}
-
+// Model viewer border style
 const modelViewer = document.querySelector('model-viewer');
 modelViewer.addEventListener('load', function(evt)
 	{
@@ -82,3 +10,12 @@ modelViewer.addEventListener('model-visibility', function(evt)
 		document.getElementById('model-viewer-wrapper').style.borderColor = '#fff';
 	}
 );
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+// Load random model
+modelNumber = getRandomInt(1, 9);
+modelPath = 'models/' + modelNumber + '.glb';
+modelViewer.setAttribute("src", modelPath);
---
+++ src/javascripts/typewriter.js
@@ -0,0 +1,71 @@
+var aText = [
+	"3d modelling",
+	"texturing",
+	"rigging",
+	"animation",
+	"python",
+	"blender",
+	"gimp",
+	"c#",
+	"hlsl",
+	"unity",
+	"html",
+	"css",
+	"javascript",
+	"typescript",
+	"web"
+	];
+	
+	var iSpeed = 100; 
+	var iIndex = 0; 
+	var iArrLength = aText[0].length; 
+	var iTextPos = 0; 
+	const archive = [];
+		
+	function typewriter()
+	{
+		var destination = document.getElementById("typewriter");
+	
+		destination.innerHTML = aText[iIndex].substring(0, iTextPos) + "_";
+		if (iTextPos++ == iArrLength)
+		{
+			iTextPos = 0;
+			const archiveText = aText[iIndex];
+			if (++iIndex == aText.length) iIndex = 0;
+			iArrLength = aText[iIndex].length;
+			setTimeout(function() {
+				pushArchive(archiveText);
+				typewriter();
+			}, 500);
+		} else
+		{
+			setTimeout(typewriter, iSpeed);
+		}
+	}
+	
+	function pushArchive(str)
+	{
+		var div = document.createElement('div');
+		div.innerText = str;
+		document.getElementById('typewriter-history').appendChild(div);
+		archive.unshift(div);
+	
+		refreshTransparency();
+	}
+	
+	function refreshTransparency()
+	{
+		for (var i = 0; i < archive.length; i++)
+		{
+			var opacity = 1 - (0.25 * (i + 1));
+			archive[i].style.opacity = opacity;
+		}
+	
+		while (archive[archive.length - 1].style.opacity <= 0)
+		{
+			archive[archive.length - 1].outerHTML = "";
+			archive.pop();
+		}
+	}
+	
+	typewriter();
