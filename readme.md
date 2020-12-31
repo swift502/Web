ce8af38e2973bcacab4adf4586714b0688981842
swift502 <blaha.j502@gmail.com>
2020-12-31 17:28:13 +0100

    character URL parameter

--- readme.md
+++ readme.md
@@ -5,3 +5,12 @@
 3. Get Bundler `gem install bundler`
 4. Run `bundle install`
 5. Run `dev` and `build` tasks
+
+## Viewing a specific model
+Pass the `character` URL parameter on the homepage to read a specific glb model. E.g.:
+
+```
+https://jblaha.art?character=3
+https://jblaha.art?character=404
+https://jblaha.art?character=boxman
+```
--- src/javascripts/homepage.js
+++ src/javascripts/homepage.js
@@ -1,9 +1,38 @@
-// Load random model
-function getRandomInt(min, max) {
+function getUrlModelNumber()
+{
+	var URLparams = getUrlParams();
+	if ('character' in URLparams) return URLparams['character'];
+	else getRandomInt(1, 9);
+}
+
+function getUrlParams()
+{
+	var url = window.location.search
+    var url = url.split('#')[0]
+    var urlParams = {}
+    var queryString = url.split('?')[1]
+    if (!queryString) {
+        if (url.search('=') !== false) {
+            queryString = url
+        }
+    }
+    if (queryString) {
+        var keyValuePairs = queryString.split('&')
+        for (var i = 0; i < keyValuePairs.length; i++) {
+            var keyValuePair = keyValuePairs[i].split('=')
+            var paramName = keyValuePair[0]
+            var paramValue = keyValuePair[1] || ''
+            urlParams[paramName] = decodeURIComponent(paramValue.replace(/\+/g, ' '))
+        }
+    }
+    return urlParams
+}
+
+function getRandomInt(min, max)
+{
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-const modelViewer = document.querySelector('model-viewer');
-modelNumber = getRandomInt(1, 9);
-modelPath = 'models/' + modelNumber + '.glb';
-modelViewer.setAttribute("src", modelPath);
+var modelNumber = getUrlModelNumber();
+var modelPath = 'models/' + modelNumber + '.glb';
+document.querySelector('model-viewer').setAttribute("src", modelPath);
