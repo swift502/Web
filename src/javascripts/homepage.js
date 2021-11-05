acce10725cbdf8e72dfcbfdd23f5ea2667c7d490
swift502 <blaha.j502@gmail.com>
2021-11-05 21:31:30 +0100

    new models

--- src/javascripts/homepage.js
+++ src/javascripts/homepage.js
@@ -2,7 +2,7 @@ function getUrlModelNumber()
 {
 	var URLparams = getUrlParams();
 	if ('character' in URLparams) return URLparams['character'];
-	else return getRandomInt(1, 9);
+	else return getRandomInt(1, 14);
 }
 
 function getUrlParams()
@@ -33,6 +33,26 @@ function getRandomInt(min, max)
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// Set model path
 var modelNumber = getUrlModelNumber();
 var modelPath = 'models/' + modelNumber + '.glb';
-document.querySelector('model-viewer').setAttribute("src", modelPath);
+document.querySelector('model-viewer').setAttribute('src', modelPath);
+
+// Set FOV
+var fovs = {
+	1: 52,
+	2: 57,
+	3: 58,
+	4: 62,
+	5: 62,
+	6: 57,
+	7: 60,
+	8: 57,
+	9: 59,
+	10: 55,
+	11: 59,
+	12: 60,
+	13: 57,
+	14: 60
+};
+document.querySelector('model-viewer').setAttribute('min-field-of-view', fovs[modelNumber] + 'deg');
