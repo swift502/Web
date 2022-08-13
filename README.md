2c4425e334fefd7662f11166245f6db275e3d284
swift502 <blaha.j502@gmail.com>
2022-08-14 00:00:53 +0200

    debug model selector

--- README.md
+++ README.md
@@ -8,13 +8,6 @@ Personal website built with [Astro](https://astro.build/).
 2. Run `npm install`
 3. Use `dev` and `build` tasks
 
-<!--
 ## Viewing a specific model
-Pass the `character` URL parameter on the homepage to read a specific glb model. E.g.:
 
-```
-https://jblaha.art?character=3
-https://jblaha.art?character=404
-https://jblaha.art?character=boxman
-```
--->
+Type `debug` on the main page to display a model selector.
---
+++ src/javascripts/model-viewer-debug.js
@@ -0,0 +1,46 @@
+let level = 0;
+let debugEnabled = false;
+
+document.addEventListener('keypress', (event) => {
+
+    if (!debugEnabled)
+    {
+        var code = event.code;
+    
+        if (level == 0 && code == 'KeyD') level++;
+        else if (level == 1 && code == 'KeyE') level++;
+        else if (level == 2 && code == 'KeyB') level++;
+        else if (level == 3 && code == 'KeyU') level++;
+        else if (level == 4 && code == 'KeyG') level++;
+        else level = 0;
+    
+        if (level == 5)
+        {
+            debugEnabled = true;
+    
+            let wrapper = document.createElement('div');
+            wrapper.id = 'model-debug-wrapper';
+            document.querySelector('.homepage').appendChild(wrapper);
+
+            let list = document.createElement('div');
+            list.id = 'model-debug-list';
+            wrapper.appendChild(list);
+
+            Object.keys(window.modelSettings).forEach(item => {
+                let button = document.createElement('div');
+                button.classList.add('tile');
+                button.innerHTML += item;
+                button.onclick = () => {
+                    document.querySelector('model-viewer').setAttribute('src', `models/${item}.glb`);
+                };
+
+                list.appendChild(button);
+            });
+
+            setTimeout(() => {
+                wrapper.style.opacity = 1;
+            }, 0);
+        }
+    }
+
+}, false);
