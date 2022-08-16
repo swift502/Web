f6362ff0a385c71df0af28a9e0ad88fcd84becdd
swift502 <blaha.j502@gmail.com>
2022-08-16 22:49:59 +0200

    image fading in

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -43,11 +43,13 @@ function fadeIn(el)
 	el.style.opacity = 0;
 	el.style.display = "block";
 
-	(function fade() {
-	  let val = parseFloat(el.style.opacity);
-	  if (!((val += .1) > 1)) {
-		el.style.opacity = val;
-		requestAnimationFrame(fade);
-	  } else {el.style.opacity = 1;}
+	(function fade()
+	{
+		let val = parseFloat(el.style.opacity);
+		if (!((val += .1) > 1))
+		{
+			el.style.opacity = val;
+			requestAnimationFrame(fade);
+		} else { el.style.opacity = 1; }
 	})();
 }
