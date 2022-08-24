5f1b0cbbf2b42b0de792ec12e5d140f29993f779
swift502 <blaha.j502@gmail.com>
2022-08-24 22:20:41 +0200

    base test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jblaha.art',
+  base: '/Web',
   vite: {
     plugins: [yaml()]
   },
