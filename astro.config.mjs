54f115e8d3bca287be4411f7ef2887fb4aacc157
swift502 <blaha.j502@gmail.com>
2022-09-12 17:23:59 +0200

    fix trailing slashes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jblaha.art',
+  trailingSlash: 'always',
   vite: {
     plugins: [yaml()]
   },
