54e375cc692134ad307c0a9d585117959fcddd66
Jan Bláha <blaha.j502@gmail.com>
2023-12-30 18:09:26 +0100

    remove IE warning

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,8 +1,3 @@
-if (window.navigator.userAgent.match(/(MSIE|Trident)/))
-{
-	alert("You seem to be using an outdated web browser and the website may not display correctly. Please view this website in a modern web browser for it to display as intended.");
-}
-
 function fadeIn(element)
 {
 	element.style.opacity = 1;
