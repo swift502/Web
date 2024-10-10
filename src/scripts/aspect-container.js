542b95219b87f5d445d1018411e73845422cf94c
Jan Bláha <blaha.j502@gmail.com>
2024-10-10 17:01:26 +0200

    indentation

--- src/scripts/aspect-container.js
+++ src/scripts/aspect-container.js
@@ -3,55 +3,55 @@ let descs = document.getElementsByClassName("content-description");
 
 function updateAspectContainers()
 {
-    for (let i = 0; i < aspectContainers.length; i++)
-    {
-        let container = aspectContainers[i];
-        let aspectRatio = container.dataset.width / container.dataset.height;
+	for (let i = 0; i < aspectContainers.length; i++)
+	{
+		let container = aspectContainers[i];
+		let aspectRatio = container.dataset.width / container.dataset.height;
 
-        let padding = 180;
-        if (container.dataset.first_padding === 'true') padding += 100;
-        if (container.dataset.desc_padding === 'true') padding += 100;
-        if (container.dataset.links_padding === 'true') padding += 100;
+		let padding = 180;
+		if (container.dataset.first_padding === 'true') padding += 100;
+		if (container.dataset.desc_padding === 'true') padding += 100;
+		if (container.dataset.links_padding === 'true') padding += 100;
 
-        let width, height;
+		let width, height;
 
-        if (window.innerWidth > 776)
-        {
-            //Desktop
-            let maxWidth = container.parentElement.offsetWidth;
-            height = Math.max(window.innerHeight - padding, 400);
-            width = (height * aspectRatio);
+		if (window.innerWidth > 776)
+		{
+			//Desktop
+			let maxWidth = container.parentElement.offsetWidth;
+			height = Math.max(window.innerHeight - padding, 400);
+			width = (height * aspectRatio);
 
-            if (width > maxWidth)
-            {
-                width = maxWidth;
-                height = width / aspectRatio;
-            }
-        }
-        else
-        {
-            // Mobile
-            width = document.documentElement.clientWidth;
-            height = width / aspectRatio;
-        }
+			if (width > maxWidth)
+			{
+				width = maxWidth;
+				height = width / aspectRatio;
+			}
+		}
+		else
+		{
+			// Mobile
+			width = document.documentElement.clientWidth;
+			height = width / aspectRatio;
+		}
 
-        container.style.height = height + 'px';
-        container.style.width = width + 'px';
-    }
+		container.style.height = height + 'px';
+		container.style.width = width + 'px';
+	}
 
-    // Descriptions update
-    for (let i = 0; i < descs.length; i++)
-    {
-        let desc = descs[i];
-        let target = document.getElementById(desc.dataset.target);
-        desc.style.width = `${target.getBoundingClientRect().width}px`;
-    }
+	// Descriptions update
+	for (let i = 0; i < descs.length; i++)
+	{
+		let desc = descs[i];
+		let target = document.getElementById(desc.dataset.target);
+		desc.style.width = `${target.getBoundingClientRect().width}px`;
+	}
 }
 
 // Update on window resize
 window.addEventListener('resize', () =>
 {
-    updateAspectContainers();
+	updateAspectContainers();
 });
 
 // Initial update
--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -3,32 +3,32 @@ let prevScrollpos = window.scrollY;
 
 window.addEventListener('scroll', () =>
 {
-    let navbar = document.getElementById("navbar");
+	let navbar = document.getElementById("navbar");
 
-    if (touchDevice)
-    {
-        let currentScrollPos = window.scrollY;
-        if (prevScrollpos > currentScrollPos)
-        {
-            navbar.style.top = "0";
-        }
-        else
-        {
-            navbar.style.top = "-64px";
-        }
-        prevScrollpos = currentScrollPos;
-    }
+	if (touchDevice)
+	{
+		let currentScrollPos = window.scrollY;
+		if (prevScrollpos > currentScrollPos)
+		{
+			navbar.style.top = "0";
+		}
+		else
+		{
+			navbar.style.top = "-64px";
+		}
+		prevScrollpos = currentScrollPos;
+	}
 
-    let navbarShadowClass = "shadow";
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0)
-    {
-        if (!navbar.classList.contains(navbarShadowClass))
-        {
-            navbar.classList.add(navbarShadowClass);
-        }
-    }
-    else if (navbar.classList.contains(navbarShadowClass))
-    {
-        navbar.classList.remove(navbarShadowClass);
-    }
+	let navbarShadowClass = "shadow";
+	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0)
+	{
+		if (!navbar.classList.contains(navbarShadowClass))
+		{
+			navbar.classList.add(navbarShadowClass);
+		}
+	}
+	else if (navbar.classList.contains(navbarShadowClass))
+	{
+		navbar.classList.remove(navbarShadowClass);
+	}
 });
