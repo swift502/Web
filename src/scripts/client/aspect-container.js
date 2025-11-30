df3a4260483884b9e671a7434fe5f89a4c4564bd
Jan Bláha <blaha.j502@gmail.com>
2025-11-30 22:26:54 +0100

    simplify inset logic

--- src/scripts/client/aspect-container.js
+++ src/scripts/client/aspect-container.js
@@ -1,22 +1,15 @@
 let aspectContainers = document.getElementsByClassName("aspect-container");
 let descs = document.getElementsByClassName("content-description");
 
-function getInsetHeight(width, height, container)
+function getInsetHeight(width, aspectRatio, container)
 {
 	let padding = getComputedStyle(container).padding;
-	padding = padding.replace('px', '');
-	console.log(padding);
-	padding = parseInt(padding) * 2;
+	padding = parseFloat(padding) * 2;
 
-	const aspectRatio = width / height;
 	const insetWidth = width - padding;
 	const insetHeight = insetWidth / aspectRatio;
-
-	const newWidth = insetWidth + padding;
-	const newHeight = insetHeight + padding;
-	const newAspectRatio = newWidth / newHeight;
-
-	return width / newAspectRatio;
+	
+	return insetHeight + padding;
 }
 
 function updateAspectContainers()
@@ -47,7 +40,7 @@ function updateAspectContainers()
 			}
 
 			if (container.dataset.inset === 'true')
-				height = getInsetHeight(width, height, container);
+				height = getInsetHeight(width, aspectRatio, container);
 
 			container.style.width = width + 'px';
 			container.style.height = height + 'px';
@@ -61,7 +54,7 @@ function updateAspectContainers()
 			height = width / aspectRatio;
 
 			if (container.dataset.inset === 'true')
-				height = getInsetHeight(width, height, container);
+				height = getInsetHeight(width, aspectRatio, container);
 
 			container.style.height = height + 'px';
 		}
